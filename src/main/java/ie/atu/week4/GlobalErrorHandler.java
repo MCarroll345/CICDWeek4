b05b2283587f24b0e982673cbb13da31d3548239
Mark Carroll <G00419267@atu.ie>
2024-10-09 15:08:52 +0100

    Person controller completed

---
+++ src/main/java/ie/atu/week4/GlobalErrorHandler.java
@@ -0,0 +1,17 @@
+package ie.atu.week4;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalErrorHandler {
+
+
+
+}
